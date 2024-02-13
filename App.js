85c9298989f955bcbaacb54e8ec134c1aaf2157f
Viduminiiii <vidumini.dayananda@gmail.com>
2024-02-14 01:55:04 +0530

    Vet digital prescription interface design

--- App.js
+++ App.js
@@ -1,16 +1,16 @@
 import React from 'react';
 // import UserCategory from './User Category';
 // import DocChannelling from './Doctor channelling'
-import Prescription from './Digital prescription'
-// import VetPrescription from './Vet - Digital prescription'
+// import Prescription from './Digital prescription'
+import VetPrescription from './Vet - Digital prescription'
 // import VetMenu from './Vet - Menu'
 
 const App = () => {
    return (
       // <DocChannelling />
       // <UserCategory />
-      <Prescription />
-      // <VetPrescription />
+      // <Prescription />
+      <VetPrescription />
       // <VetMenu />
    )
 }
