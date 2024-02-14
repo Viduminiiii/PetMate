7ad4e2b348cdaf852b4c270e7106691d3896216f
ChathilaV <chathila.20220698@iit.ac.lk>
2024-02-14 15:06:43 +0530

    Pet owner menu incomplete

--- App.js
+++ App.js
@@ -2,16 +2,20 @@ import React from 'react';
 // import UserCategory from './User Category';
 // import DocChannelling from './Doctor channelling'
 // import Prescription from './Digital prescription'
-import VetPrescription from './Vet - Digital prescription'
+// import VetPrescription from './Vet - Digital prescription'
 // import VetMenu from './Vet - Menu'
+import PetOwnerMenu from './petowner_menu'
+
 
 const App = () => {
    return (
       // <DocChannelling />
       // <UserCategory />
       // <Prescription />
-      <VetPrescription />
+      // <VetPrescription />
       // <VetMenu />
+      <PetOwnerMenu />
+
    )
 }
 export default App
