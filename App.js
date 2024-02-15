6f697a78c0c61801f98d36009ec1a68edbda142a
Hesadunie <cwijeginasinghe@gmail.com>
2024-02-15 22:33:48 +0530

    Login interface design

--- App.js
+++ App.js
@@ -1,21 +1,23 @@
 import React from 'react';
 // import UserCategory from './User Category';
-// import DocChannelling from './Doctor channelling'
+// import DocChannelling from './Doctor channelling';
 // import Prescription from './Digital prescription'
 // import VetPrescription from './Vet - Digital prescription'
 // import VetMenu from './Vet - Menu'
-import PetOwnerMenu from './petowner_menu'
+// import PetOwnerMenu from './petowner_menu'
+import Login from './Login';
 
 
-const App = () => {
+function App() {
    return (
       // <DocChannelling />
       // <UserCategory />
       // <Prescription />
       // <VetPrescription />
       // <VetMenu />
-      <PetOwnerMenu />
+      // <PetOwnerMenu />
+      <Login />
 
-   )
+   );
 }
 export default App
