e87e9bf90335c2cbfdfca40dcf2abcd8160a066c
Hasitha-Prabath2002 <hpkatugampola2002@gmail.com>
2024-03-18 10:15:17 +0530

    Make some changes in region and styles in Google Map

--- Frontend/GoogleMap.js
+++ Frontend/GoogleMap.js
@@ -1,5 +1,9 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
+/*
+Use to embed interactive maps into the application
+PROVIDER_GOOGLE - Specifies that the Google Maps API is use as the map provider
+*/
 import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
 
 const GoogleMap = () => {
@@ -9,10 +13,10 @@ const GoogleMap = () => {
         provider={PROVIDER_GOOGLE}
         style={styles.map}
         region={{
-          latitude: 37.78825,
-          longitude: -122.4324,
-          latitudeDelta: 0.015,
-          longitudeDelta: 0.0121,
+          latitude: 6.9265707279464, // Indicates the north-south position on the Earth's surface.
+          longitude: 79.86142018841134, // Indicates the east-west position on the Earth's surface.
+          latitudeDelta: 0.02, // Indicates the zoom level of the map along the latitude axis.
+          longitudeDelta: 0.02, // Indicates the zoom level of the map along the longitude axis.
         }}
       ></MapView>
     </View>
@@ -21,13 +25,20 @@ const GoogleMap = () => {
 
 const styles = StyleSheet.create({
   container: {
+    /*
+    This sets the positioning properties to "absolute" and the top, bottom, left and right
+    properties to 0 for effectively cover the entire map container.
+    */
     ...StyleSheet.absoluteFillObject,
-    height: 400,
-    width: 400,
+    flex: 1, // Use to fill the all available space.
     justifyContent: "flex-end",
     alignItems: "center",
   },
   map: {
+    /*
+    This sets the positioning properties to "absolute" and the top, bottom, left and right
+    properties to 0 for effectively cover the entire map.
+    */
     ...StyleSheet.absoluteFillObject,
   },
 });
