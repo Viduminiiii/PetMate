daa7b440cd1b930c165f5d7ffc56504971d660c8
Hasitha-Prabath2002 <hpkatugampola2002@gmail.com>
2024-03-19 14:32:57 +0530

    Added function for move to the user searched location

--- Frontend/GoogleMap.js
+++ Frontend/GoogleMap.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef } from "react";
 import { View, Text, StyleSheet } from "react-native";
 import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
 /*
@@ -8,23 +8,47 @@ PROVIDER_GOOGLE - Specifies that the Google Maps API is use as the map provider
 import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
 
 const GoogleMap = () => {
+  const mapRef = useRef(null);
+  /*
+  This function is use to animating the map view to specific location
+  defined by the provided latitude and longitude.
+  */
+  const moveToTheLocation = (latitude, longitude) => {
+    mapRef.current.animateToRegion(
+      {
+        latitude,
+        longitude,
+        latitudeDelta: 0.007, // Indicates the zoom level of the map along the latitude axis.
+        longitudeDelta: 0.007, // Indicates the zoom level of the map along the longitude axis.
+      },
+      2000 // The duration of the animation is set to 2000 milliseconds (2 seconds)
+    );
+  };
   return (
     <View style={styles.container}>
       <MapView
+        ref={mapRef}
         provider={PROVIDER_GOOGLE}
         style={styles.map}
         region={{
           latitude: 6.9265707279464, // Indicates the north-south position on the Earth's surface.
           longitude: 79.86142018841134, // Indicates the east-west position on the Earth's surface.
-          latitudeDelta: 0.02, // Indicates the zoom level of the map along the latitude axis.
-          longitudeDelta: 0.02, // Indicates the zoom level of the map along the longitude axis.
+          latitudeDelta: 0.07, // Indicates the zoom level of the map along the latitude axis.
+          longitudeDelta: 0.07, // Indicates the zoom level of the map along the longitude axis.
         }}
       />
       <View style={styles.google_map_places}>
         <GooglePlacesAutocomplete
           placeholder="Search"
+          fetchDetails={true} // Fetch additional details to the selected place.
           onPress={(data, details = null) => {
-            console.log(data.details);
+            // Get the details of the selected place's latitude and longitude to the console.
+            console.log(JSON.stringify(details?.geometry?.location));
+            // Call the moveToTheLocation function and pass the lat and lng.
+            moveToTheLocation(
+              details?.geometry?.location.lat,
+              details?.geometry?.location.lng
+            );
           }}
           query={{
             key: "AIzaSyBoVi87Vq42Lj9ZdkJpEdSMGahP9pWFvwY", // Include Google Map API key for the application.
