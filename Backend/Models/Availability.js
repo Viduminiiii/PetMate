32abaead9968476a2558d47021dd8c10a40aee8b
ChathilaV <chathila.20220698@iit.ac.lk>
2024-03-28 13:20:10 +0530

    Availability database model changes

--- Backend/Models/Availability.js
+++ Backend/Models/Availability.js
@@ -8,14 +8,15 @@ const AvailabilitySchema=new Schema({
     timeFrom: { type: Date, required: true }, // Start time of the availability 
     timeTo: { type: Date, required: true }, // End time of the availability 
     noofPatients: { type: Number, required: true }, // Number of patients that can be accommodated during this availability slot
-    doctorCharges: { type: Number, required: true },
-    serviceCharges: { type: Number, required: true },
-    createdDate: {type: Date, required: true},
-    modifiedDate: {type: Date },
-    veternarian: { type: Schema.Types.ObjectId, ref: 'Veternarian' }
+    doctorCharges: { type: Number, required: true }, // Charges for the doctor's service during this availability slot
+    serviceCharges: { type: Number, required: true }, // Additional service charges during this availability slot
+    createdDate: {type: Date, required: true}, // Date when the availability was created
+    modifiedDate: {type: Date }, // Date when the availability was last modified
+    veternarian: { type: Schema.Types.ObjectId, ref: 'Veternarian' } // Reference to the veterinarian associated with this availability
 },{
-    collection: "Availability"
+    collection: "Availability" // Setting the collection name explicitly to "Availability"
 })
+// Creating a model named "Availability" based on the schema
 const Availability = mongoose.model("Availability", AvailabilitySchema);
-
+// Exporting the model
 module.exports = Availability;
