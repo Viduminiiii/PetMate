02c9690b1e1a37c32d726d9de43b8b4d0fcb2ae9
VimanshiUH <vimanshivishwamani2@gmail.com>
2024-03-29 01:35:29 +0530

    Chat messages component added

--- Frontend/components/ChatMessage.js
+++ Frontend/components/ChatMessage.js
@@ -1,430 +1,122 @@
 import {
-    StyleSheet,
-    Text,
-    View,
-    ScrollView,
-    KeyboardAvoidingView,
-    TextInput,
-    Pressable,
-    Image,
-  } from "react-native";
-  import React, {
-    useState,
-    useLayoutEffect,
-    useEffect,
-    useRef,
-  } from "react";
-  import { Feather } from "@expo/vector-icons";
-  import { Ionicons } from "@expo/vector-icons";
-  import { FontAwesome } from "@expo/vector-icons";
-  import { MaterialIcons } from "@expo/vector-icons";
-  import { Entypo } from "@expo/vector-icons";
-  import EmojiSelector from "react-native-emoji-selector";
-  import { useAuth } from "../config/AuthContext";
-  import { useNavigation, useRoute } from "@react-navigation/native";
-  import * as ImagePicker from "expo-image-picker";
-  import axios from "axios";
-  
-  const imgSource = require("../../AppPics/Dog.png");
-  const config = require("../config/config");
-  
-  const ChatMessagesScreen = () => {
-    const baseURL = config.DB_HOST + ":" + config.DB_PORT;
-    //   console.log("baseURL: " + baseURL);
-  
-    const navigation = useNavigation();
-    const { user, userID, userType } = useAuth();
-    const [objUser, setObjUser] = useState();
-  
-    const [showEmojiSelector, setShowEmojiSelector] = useState(false);
-    const [selectedMessages, setSelectedMessages] = useState([]);
-    const [messages, setMessages] = useState([]);
-    const [recepientData, setRecepientData] = useState();
-    const [selectedImage, setSelectedImage] = useState("");
-    const route = useRoute();
-    const { recepientId } = route.params;
-    const [message, setMessage] = useState("");
-    const scrollViewRef = useRef(null);
-  
-    useEffect(() => {
-      // console.log("\n----user:  " + JSON.stringify(user));
-      // console.log("\n------ recepientId:   " + recepientId);
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+  Image,
+} from "react-native";
+import React, { useLayoutEffect, useContext, useEffect, useState } from "react";
+import { useNavigation } from "@react-navigation/native";
+import { Ionicons } from "@expo/vector-icons";
+import { MaterialIcons } from "@expo/vector-icons";
+import { jwtDecode } from "jwt-decode";
+import axios from "axios";
+import { useAuth } from "./config/AuthContext";
+const config = require("./config/config");
+
+const ChatScreen = () => {
+  const baseURL = config.DB_HOST + ":" + config.DB_PORT;
+  // console.log("baseURL: " + baseURL);
+
+  const navigation = useNavigation();
+  const { login, user, userID, userType } = useAuth();
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
       const userObject = JSON.parse(user);
-      setObjUser(userObject);
-      scrollToBottom();
-  
-      
-      fetchRecepientData();
-      fetchMessages();
-    }, []);
-  
-    const scrollToBottom = () => {
-      if (scrollViewRef.current) {
-        scrollViewRef.current.scrollToEnd({ animated: false });
-      }
-    };
-  
-    const handleContentSizeChange = () => {
-      scrollToBottom();
-    };
-  
-    const handleEmojiPress = () => {
-      setShowEmojiSelector(!showEmojiSelector);
-    };
-  
-    const fetchMessages = async () => {
-      // console.log("\n---------------objUser: " + JSON.stringify(objUser));
-      console.log("\n------- fetch userID 1:    " + userID);
-      // console.log("\n------fetch recepientId:   " + recepientId);
-      try {
-        axios
-          .get(`${baseURL}/messages/${userID}/${recepientId}`)
-          .then((res) => {
-            console.log("\n---- setMessages fetchMessages:  " + JSON.stringify(res.data));
-            setMessages(res.data);
-          })
-          .catch((e) => console.log(e));
-      } catch (error) {
-        console.log("error 1 fetching messages", error);
-      }
-    };
-    
-    const fetchRecepientData = async () => {
-      try {
-        
+      const token = userObject.token;
       axios
-      .get(`${baseURL}/user/${recepientId}`)
-      .then((res) => {
-        // console.log("\n---- setMessages fetchRecepientData:  " + JSON.stringify(res.data));
-        setRecepientData(res.data);
-      })
-      .catch((e) => console.log(e));
-      } catch (error) {
-        console.log("error 2 retrieving details", error);
-      }
+        .get(baseURL + `/users/${userID}/${userType}`)
+        .then((res) => {
+          setUsers(res.data);
+        })
+        .catch((e) => console.log(e));
     };
-  
-  
-    const handleSend = async (messageType, imageUri) => {
-      // console.log("------------userID 2:    " + objUser.userID);
-      try {    
-        const formData = {
-          senderId: userID,
-          recepientId: recepientId,
-          messageType: messageType === "image" ? "image" : "text",
-          imageFile: { uri: imageUri, name: "image.jpg", type: "image/jpeg" },
-          messageText: message,
-        };
-  
-        axios
-          .post(baseURL + "/messages", formData)
-          .then((res) => {
-            // console.log(res.data);
-            if (res.data.status === 200) {
-              setMessage("");
-              setSelectedImage("");
-              fetchMessages();
-            }
-          })
-          .catch((e) => console.log(e));
-      } catch (error) {
-        console.log("error 3 in sending the message", error);
-      }
-    };
-  
-  //   console.log("messages", selectedMessages);
-    useLayoutEffect(() => {
-      navigation.setOptions({
-        headerTitle: "",
-        headerLeft: () => (
-          <View style={{ flexDirection: "row", alignItems: "center", gap: 10 }}>
-            <Ionicons
-              onPress={() => navigation.goBack()}
-              name="arrow-back"
-              size={24}
-              color="black"
-            />
-  
-            {selectedMessages.length > 0 ? (
+
+    fetchUsers();
+  }, []);
+
+  return (
+    <View>
+      <Text
+        style={{
+          fontSize: 28,
+          fontWeight: "bold",
+          marginLeft: 10,
+          marginTop: 10,
+          textDecorationLine: "underline",
+        }}
+      >
+        Chat Room
+      </Text>
+      <View style={{ padding: 10 }}>
+        {users &&
+          users.map((item) => (
+            <TouchableOpacity
+              key={item._id}
+              onPress={() =>
+                {
+                  navigation.navigate("ChatMessages", { recepientId: item._id });
+                  // alert("---- recepientId  " + item._id);
+              }
+              }
+              style={{
+                flexDirection: "row",
+                alignItems: "center",
+                marginVertical: 10,
+                borderRadius: 20,
+                borderColor: "rgba(128, 128, 128, 0.2)", // Gray color with 20% opacity
+                borderWidth: 3, // Optional: Set border width
+              }}
+            >
               <View>
-                <Text style={{ fontSize: 16, fontWeight: "500" }}>
-                  {selectedMessages.length}
-                </Text>
-              </View>
-            ) : (
-              <View style={{ flexDirection: "row", alignItems: "center" }}>
                 <Image
                   style={{
-                    width: 30,
-                    height: 30,
-                    borderRadius: 15,
+                    width: 80,
+                    height: 80,
+                    borderRadius: 50,
+                    margin: 3,
                     resizeMode: "cover",
+                    borderColor: "rgba(128, 128, 128, 0.3)", // Gray color with 30% opacity
+                    borderWidth: 3, // Optional: Set border width
                   }}
-                  source={imgSource} //{{ uri: recepientData?.image }}
+                  source={require("../AppPics/Dog.png")}
                 />
-  
-                <Text style={{ marginLeft: 5, fontSize: 15, fontWeight: "bold" }}>
-                  {recepientData?.name}
+              </View>
+              <View style={{ marginLeft: 20, flex: 1 }}>
+                <Text style={{ fontSize: 15, fontWeight: "bold" }}>
+                  {item?.Veternarian
+                    ? item?.Veternarian.fullname
+                    : item?.petOwner.fullname}
+                </Text>
+                <Text style={{ fontSize: 14 }}>
+                  {item?.Veternarian
+                    ? item?.Veternarian.veterinaryClinicName
+                    : item?.petOwner.petname}
+                </Text>
+                <Text style={{ marginTop: 2, color: "gray" }}>
+                  {item?.Veternarian
+                    ? item?.Veternarian.email
+                    : item?.petOwner.email}
                 </Text>
               </View>
-            )}
-          </View>
-        ),
-        headerRight: () =>
-          selectedMessages.length > 0 ? (
-            <View style={{ flexDirection: "row", alignItems: "center", gap: 10 }}>
-              <Ionicons name="md-arrow-redo-sharp" size={24} color="black" />
-              <Ionicons name="md-arrow-undo" size={24} color="black" />
-              <FontAwesome name="star" size={24} color="black" />
-              <MaterialIcons
-                onPress={() => deleteMessages(selectedMessages)}
-                name="delete"
-                size={24}
-                color="black"
-              />
-            </View>
-          ) : null,
-      });
-    }, [recepientData, selectedMessages]);
-  
-    const deleteMessages = async (messageIds) => {
-      try {
-        const response = await fetch(baseURL + "/deleteMessages", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({ messages: messageIds }),
-        });
-  
-        if (response.ok) {
-          setSelectedMessages((prevSelectedMessages) =>
-            prevSelectedMessages.filter((id) => !messageIds.includes(id))
-          );
-  
-          fetchMessages();
-        } else {
-          console.log("error 4 deleting messages", response.status);
-        }
-      } catch (error) {
-        console.log("error 5 deleting messages", error);
-      }
-    };
-    const formatTime = (time) => {
-      const options = { hour: "numeric", minute: "numeric" };
-      return new Date(time).toLocaleString("en-US", options);
-    };
-    const pickImage = async () => {
-      let result = await ImagePicker.launchImageLibraryAsync({
-        mediaTypes: ImagePicker.MediaTypeOptions.All,
-        allowsEditing: true,
-        aspect: [4, 3],
-        quality: 1,
-      });
-  
-      console.log(result);
-      if (!result.canceled) {
-        handleSend("image", result.uri);
-      }
-    };
-    const handleSelectMessage = (message) => {
-      //check if the message is already selected
-      const isSelected = selectedMessages.includes(message._id);
-  
-      if (isSelected) {
-        setSelectedMessages((previousMessages) =>
-          previousMessages.filter((id) => id !== message._id)
-        );
-      } else {
-        setSelectedMessages((previousMessages) => [
-          ...previousMessages,
-          message._id,
-        ]);
-      }
-    };
-    return (
-      <KeyboardAvoidingView style={{ flex: 1, backgroundColor: "#F0F0F0" }}>
-        <ScrollView
-          ref={scrollViewRef}
-          contentContainerStyle={{ flexGrow: 1 }}
-          onContentSizeChange={handleContentSizeChange}
-        >
-          {messages.map((item, index) => {
-            if (item.messageType === "text") {
-              const isSelected = selectedMessages.includes(item._id);
-              return (
-                <Pressable
-                  onLongPress={() => handleSelectMessage(item)}
-                  key={index}
-                  style={[
-                    item?.senderId?._id === userID
-                      ? {
-                          alignSelf: "flex-end",
-                          backgroundColor: "#DCF8C6",
-                          padding: 8,
-                          maxWidth: "60%",
-                          borderRadius: 7,
-                          margin: 10,
-                        }
-                      : {
-                          alignSelf: "flex-start",
-                          backgroundColor: "white",
-                          padding: 8,
-                          margin: 10,
-                          borderRadius: 7,
-                          maxWidth: "60%",
-                        },
-  
-                    isSelected && { width: "100%", backgroundColor: "#F0FFFF" },
-                  ]}
-                >
-                  <Text
-                    style={{
-                      fontSize: 13,
-                      textAlign: isSelected ? "right" : "left",
-                    }}
-                  >
-                    {item?.message}
-                  </Text>
-                  <Text
-                    style={{
-                      textAlign: "right",
-                      fontSize: 9,
-                      color: "gray",
-                      marginTop: 5,
-                    }}
-                  >
-                    {formatTime(item.timeStamp)}
-                  </Text>
-                </Pressable>
-              );
-            }
-  
-            if (item.messageType === "image") {
-              const baseUrl = "Backend/files"; //"/Users/sujananand/Build/messenger-project/api/files/";
-              const imageUrl = item.imageUrl;
-              const filename = imageUrl.split("/").pop();
-              // const source = { uri: baseUrl + filename };
-              const source = require("../../AppPics/Dog.png"); //{ uri: baseUrl + filename };
-              return (
-                <Pressable
-                  key={index}
-                  style={[
-                    item?.senderId?._id === userID
-                      ? {
-                          alignSelf: "flex-end",
-                          backgroundColor: "#DCF8C6",
-                          padding: 8,
-                          maxWidth: "60%",
-                          borderRadius: 7,
-                          margin: 10,
-                        }
-                      : {
-                          alignSelf: "flex-start",
-                          backgroundColor: "white",
-                          padding: 8,
-                          margin: 10,
-                          borderRadius: 7,
-                          maxWidth: "60%",
-                        },
-                  ]}
-                >
-                  <View>
-                    <Image
-                      source={source}
-                      style={{ width: 200, height: 200, borderRadius: 7 }}
-                    />
-                    <Text
-                      style={{
-                        textAlign: "right",
-                        fontSize: 9,
-                        position: "absolute",
-                        right: 10,
-                        bottom: 7,
-                        color: "white",
-                        marginTop: 5,
-                      }}
-                    >
-                      {formatTime(item?.timeStamp)}
-                    </Text>
-                  </View>
-                </Pressable>
-              );
-            }
-          })}
-        </ScrollView>
-  
-        <View
-          style={{
-            flexDirection: "row",
-            alignItems: "center",
-            paddingHorizontal: 10,
-            paddingVertical: 10,
-            borderTopWidth: 1,
-            borderTopColor: "#dddddd",
-            marginBottom: showEmojiSelector ? 0 : 25,
-          }}
-        >
-          <Entypo
-            onPress={handleEmojiPress}
-            style={{ marginRight: 5 }}
-            name="emoji-happy"
-            size={24}
-            color="gray"
-          />
-  
-          <TextInput
-            value={message}
-            onChangeText={(text) => setMessage(text)}
-            style={{
-              flex: 1,
-              height: 40,
-              borderWidth: 1,
-              borderColor: "#dddddd",
-              borderRadius: 20,
-              paddingHorizontal: 10,
-            }}
-            placeholder="Type Your message..."
-          />
-  
-          <View
-            style={{
-              flexDirection: "row",
-              alignItems: "center",
-              gap: 7,
-              marginHorizontal: 8,
-            }}
-          >
-            <Entypo onPress={pickImage} name="camera" size={24} color="gray" />
-  
-            <Feather name="mic" size={24} color="gray" />
-          </View>
-  
-          <Pressable
-            onPress={() => handleSend("text")}
-            style={{
-              backgroundColor: "#007bff",
-              paddingVertical: 8,
-              paddingHorizontal: 12,
-              borderRadius: 20,
-            }}
-          >
-            <Text style={{ color: "white", fontWeight: "bold" }}>Send</Text>
-          </Pressable>
-        </View>
-  
-        {showEmojiSelector && (
-          <EmojiSelector
-            onEmojiSelected={(emoji) => {
-              setMessage((prevMessage) => prevMessage + emoji);
-            }}
-            style={{ height: 250 }}
-          />
-        )}
-      </KeyboardAvoidingView>
-    );
-  };
-  
-  export default ChatMessagesScreen;
-  
-  const styles = StyleSheet.create({});
+              <MaterialIcons name="navigate-next" size={50} color="black" />
+            </TouchableOpacity>
+          ))}
+      </View>
+    </View>
+  );
+};
+
+export default ChatScreen;
+
+const styles = StyleSheet.create({
+  icon1: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 8,
+  },
+  text1: { fontSize: 16, fontWeight: "bold" },
+});
