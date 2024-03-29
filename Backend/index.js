2e2907fc6e3627a41ef8b5083b5cd7a8ced17b4c
Hesadunie <cwijeginasinghe@gmail.com>
2024-03-29 06:58:54 +0530

    locate vet clinics API

--- Backend/index.js
+++ Backend/index.js
@@ -554,6 +554,34 @@ app.post("/searchAvailability", async (req, res) => {
   }
 });
 
+app.post("/searchClinic", async (req, res) => {
+  console.log("-------------REQ BODY" + JSON.stringify(req.body));
+  const { searchClinic } = req.body; // Assuming vet ID is passed as a query parameter
+  try {
+    if (isValidSearchRegExp(searchClinic)) {
+      const regexClin = new RegExp(searchClinic, "i");
+      // console.log("RegExp: ", regexClin);
+
+      const veternarians = await Veternarian.find({
+        mainCity: { $regex: regexClin },
+      }).select(
+        "_id fullname veterinaryClinicName mainCity veterinaryClinicAddress"
+      );
+      console.log("veternarians: " + JSON.stringify(veternarians));
+
+      if (veternarians && veternarians.length > 0) {
+        res.send(JSON.stringify(veternarians));
+      } else {
+        console.log("Status:    " + res.status);
+        res.status(404).send("Date Data not found.");
+      }
+    }
+  } catch (error) {
+    console.error("Error during database query:", error);
+    res.status(500).send({ status: "Error", data: error.message });
+  }
+});
+
 ///// STRIPE Start
 
 function getKeys(keyType) {
@@ -581,21 +609,6 @@ app.get("/stripe-key", (req, res) => {
   return res.send({ data: publishable_key });
 });
 
-// const userID = async (username, res) => {
-//   console.log("username:   " + username);
-//   try {
-//     const result = await User.findOne({ username: username });
-//     console.log("result:   " + JSON.stringify(result._id));
-//     if (result) {
-//       res.send({ data: result._id });
-//     } else {
-//       res.send({ error: "User did not match." });
-//     }
-//   } catch (error) {
-//     console.error("An error occurred:", error);
-//     res.send({ error: "An error occurred while fetching user data." });
-//   }
-// };
 
 async function GetUserID(username) {
   console.log("username:   " + username);
@@ -631,11 +644,7 @@ app.post("/create-payment-intent", async (req, res) => {
 
   try {
     console.log("----3----  " + orderAmount + " - " + currency);
-    // const paymentIntent = await stripe.paymentIntents.create(params)
-    // .then((res) => {
-    //   if(res)
-    //   {console.log("res");}
-    // });
+    
     const paymentIntent = await stripe.paymentIntents.create({
       amount: orderAmount,
       currency: currency,
