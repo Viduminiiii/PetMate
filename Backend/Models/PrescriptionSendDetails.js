52c6c02ff6b18e1ffffd9b96a1048eae38771894
ChathilaV <chathila.20220698@iit.ac.lk>
2024-03-29 09:37:31 +0530

    PrescriptionSendDetails database model changes

--- Backend/Models/PrescriptionSendDetails.js
+++ Backend/Models/PrescriptionSendDetails.js
@@ -1,6 +1,9 @@
+// Importing mongoose library
 const mongoose =require('mongoose');
+// Destructuring Schema from mongoose
 const { Schema } = mongoose;
 
+// Defining schema for prescription send details
 const PrescriptionSendDetailsSchema=new Schema({
     appointments: { type: Schema.Types.ObjectId, ref: 'Appointments' },
     sendPharmacy: { type: Schema.Types.ObjectId, ref: 'Pharmacy' },
@@ -11,8 +14,9 @@ const PrescriptionSendDetailsSchema=new Schema({
     createdDate: {type: Date, required: true},
     modifiedDate: {type: Date },
 },{
-    collection: "PrescriptionSendDetails"
+    collection: "PrescriptionSendDetails" // Setting the collection name explicitly to "PrescriptionSendDetails"
 })
+// Creating a model named "PrescriptionSendDetails" based on the schema
 const PrescriptionSendDetails = mongoose.model("PrescriptionSendDetails", PrescriptionSendDetailsSchema);
-
+// Exporting the model
 module.exports = PrescriptionSendDetails;
